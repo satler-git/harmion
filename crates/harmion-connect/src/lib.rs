d542a7ca8da6ad979081aa34bbe11e777582a0d6
satler <satler@satler.dev>
2025-06-27 20:37:11 +0900

    feat: add basic traits to communicate

--- crates/harmion-connect/src/lib.rs
+++ crates/harmion-connect/src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Topic, Connectionなどいろいろ考えれてないことばかり
+
+pub trait Topic {
+    fn value(&self) -> &str;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T: AsRef<str>> Topic for T {
+    fn value(&self) -> &str {
+        self.as_ref()
     }
+}
+
+// child, parent
+pub struct TopicTree(pub Vec<Box<dyn Topic>>);
+
+// topic!("/to/file" / harmion::channel::builtin::Write)
+// Write -> harmion/ ...
+// parent: None
+#[macro_export]
+macro_rules! topic {
+    ( $single:expr ) => {{
+        let mut v = Vec::new();
+        v.push(Box::new($single) as Box<dyn Topic>);
+        TopicTree(v)
+    }};
+    // 再帰ケース
+    ( $head:expr , $($rest:expr),+ ) => {{
+        let mut tree = $crate::topic_tree!($($rest)/+).0;
+        tree.push(Box::new($head) as Box<dyn Topic>);
+        TopicTree(tree)
+    }};
+}
+
+pub trait Subscriber {
+    type E: std::error::Error;
+
+    fn subscribe(&self, topic: TopicTree) -> Result<(), Self::E>;
+    fn un_subscribe(&self, topic: TopicTree) -> Result<(), Self::E>;
+}
+
+pub trait Message {}
+
+pub trait Sender {
+    type E: std::error::Error;
+
+    async fn send(&mut self, topic: TopicTree, message: impl Message) -> Result<(), Self::E>;
+}
+
+pub trait Receiver {
+    type E: std::error::Error;
+
+    fn topic<'a>() -> &'a TopicTree;
+    async fn recv(&mut self) -> Result<impl Message, Self::E>;
+}
+
+use serde::{de::DeserializeOwned, Serialize};
+
+trait Connection
+where
+    Self: Subscriber + Receiver + Sender,
+{
+    type NodeInfo: Serialize + DeserializeOwned; // SDP
+    type NodeID: Serialize + DeserializeOwned; // Node Ident
+
+    type E: std::error::Error;
+
+    fn node_info(&self) -> &(Self::NodeInfo, Self::NodeInfo);
+    fn node_list(&self) -> &[(Self::NodeInfo, Self::NodeInfo)];
+
+    async fn connect(
+        &mut self,
+        node: Self::NodeInfo,
+        node_id: Option<Self::NodeID>,
+    ) -> Result<Self::NodeID, <Self as Connection>::E>;
 }
